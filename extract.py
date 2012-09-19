737922e3daab11c6b2242c9bbcdd40e55fcd0c2f
Riccardo Iaconelli <riccardo@kde.org>
2012-09-19 11:49:08 +0200

    minimal code
    Signed-off-by: Riccardo Iaconelli <riccardo@kde.org>

--- extract.py
+++ extract.py
@@ -10,10 +10,9 @@ def funz(P, x):
     return P[0]*np.exp(-x/P[1])
 
 t = TreeHandler('out.root', 'tree')
-data = t.AbsLength
+
 h = HistoManager()
-h.set_data(data)
-h.set_initial_params([8000,1850])
+h.set_data(t.AbsLength)
 h.set_func(funz)
 h.fit_and_draw()
 h.save()
